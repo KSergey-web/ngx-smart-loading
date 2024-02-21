bad46487fc897aeff40167b1786357453bfe208e
kulaevsu <kulaevsu@cifra-k.ru>
2024-02-22 02:16:01 +0700

    feat: added class NgxSmartLoadingService for manage loading state

--- projects/ngx-smart-loading/src/lib/ngx-smart-loading.service.ts
+++ projects/ngx-smart-loading/src/lib/ngx-smart-loading.service.ts
@@ -1,8 +1,120 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import { BehaviorSubject, combineLatest, finalize, map, Observable, of, Subscription, switchMap } from "rxjs";
 
+/**
+ * Main service for manage loading
+ * @constructor */
 @Injectable({
   providedIn: 'root'
 })
-export class NgxSmartLoadingService {
-  constructor() { }
+export class NgxSmartLoadingService implements OnDestroy {
+  /**
+   * The loader is tracked by default
+   */
+  private _defaultLoader = new BehaviorSubject(false);
+  /**
+   * All incomplete loaders added to the service
+   */
+  private _loaders: Observable<boolean>[] = [];
+  /**
+   * Subscription on multiplexing loaders
+   */
+  private _subscription: Subscription = new Subscription();
+
+  constructor() {
+    this.observeDefaultLoaderWithStack();
+  }
+
+  /**
+   * This subject contains summary information about whether
+   * all loaders have finished loading in the {@link NgxSmartLoadingService._loaders} variable
+   */
+  private _isFetching$ = new BehaviorSubject(false);
+
+  /**
+   * This observable contains summary information about loading {@link NgxSmartLoadingService._isFetching$}
+   */
+  get isFetching$(): Observable<boolean> {
+    return this._isFetching$.asObservable();
+  }
+
+  /**
+   * This value contains summary information about loading {@link NgxSmartLoadingService._isFetching$}
+   */
+  get isFetching(): boolean {
+    return this._isFetching$.value;
+  }
+
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
+  }
+
+  /**
+   * Tell the default download that a new download has started*/
+  startLoading(): void {
+    this._defaultLoader.next(true);
+  }
+
+  /**
+   * Tell the default download that some download has ended*/
+  stopLoading(): void {
+    this._defaultLoader.next(false);
+  }
+
+  /**
+   * Can use for wrapping requests
+   * It is important that the passed observable
+   * is completed otherwise the download status will not stop
+   */
+  wrapObservable<T>(obs$: Observable<T>): Observable<T> {
+    return of(1)
+      .pipe(
+        switchMap(() => {
+          this.startLoading();
+          return obs$;
+        }),
+      )
+      .pipe(finalize(() => this.stopLoading()));
+  }
+
+  /**
+   * Changing the output state of the loader based on the stack.
+   * It gives the behavior of the loader, which will say that the download
+   * stopped only when the input values of false will be exactly as many as there were true values*/
+  addStackToLoader(loader: Observable<boolean>): Observable<boolean> {
+    let loadersCount = 0;
+    return loader.pipe(
+      map((isLoading) => {
+        if (isLoading) {
+          ++loadersCount;
+        } else {
+          loadersCount = loadersCount ? loadersCount - 1 : 0;
+        }
+        return loadersCount > 0;
+      }),
+    );
+  }
+
+  addLoaderForMultiplexing(newLoader: Observable<boolean>): void {
+    this._loaders.push(newLoader);
+    this.multiplexLoaders();
+  }
+
+  /**
+   * makes it possible to use the default loader for multiple downloads thanks to the stack */
+  private observeDefaultLoaderWithStack() {
+    this.addLoaderForMultiplexing(this.addStackToLoader(this._defaultLoader));
+  }
+
+  private multiplexLoaders(): void {
+    this._subscription.unsubscribe();
+    this._subscription = combineLatest(this._loaders)
+      .subscribe((result) => {
+        if (result.every((item) => !item)) {
+          this._isFetching$.next(false);
+        } else {
+          this._isFetching$.next(true);
+        }
+      });
+  }
 }
