c140ee7676eb29739107eb9a473d43c668c434b3
kulaevsu <kulaevsu@cifra-k.ru>
2024-03-20 00:37:08 +0700

    refactor: delete extra code

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgxSmartLoadingComponent } from "ngx-smart-loading";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgxSmartLoadingComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
