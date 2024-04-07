243207b418e811de7a02c73807cf16a6c475e22c
kulaevsu <kulaevsu@cifra-k.ru>
2024-04-07 17:55:29 +0700

    feat: added simple example to demo project

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxSmartLoadingService } from 'ngx-smart-loading';
+import { NgOptimizedImage } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgOptimizedImage],
+  providers: [NgxSmartLoadingService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'demo';
+  private loaderService: NgxSmartLoadingService = inject(
+    NgxSmartLoadingService,
+  );
+
+  isLoading = toSignal(this.loaderService.isLoading$);
+
+  startLoading() {
+    this.loaderService.startLoading();
+  }
+
+  stopLoading() {
+    this.loaderService.stopLoading();
+  }
 }
